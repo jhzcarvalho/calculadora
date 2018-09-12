9eeea316a8f5ce60ed341c338df4e308690b55ef
jhzcarvalho <ra95687@uem.br>
2018-09-12 16:41:58 -0300

    main implementada

--- main.c
+++ main.c
@@ -17,6 +17,20 @@ double div(double x , double y){
 }
 
 int main(){
+	int x, y, o;
+	printf("digite os numeros desejados");
+	scanf("%d", &x);
+	scanf("%d", &y);
+
+	printd("escolha uma operação: \n");	
+	printd("  1 - soma\n");
+	printd("  2 - subtracao\n");
+	printd("  3 - multiplicacao\n");
+	printd("  4 - divisao\n");
+	scanf ("%d" &o);		
+	
+	
+
 	return 0;
 }
  
